64613a4aef2c5d381b9fdc2945ea2456071a421a
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-05-21 19:04:34 +0700

    feat: slicing login page

--- app/src/main/java/com/cpp/unsmoke/utils/customview/CustomButton.kt
+++ app/src/main/java/com/cpp/unsmoke/utils/customview/CustomButton.kt
@@ -1,4 +1,42 @@
 package com.cpp.unsmoke.utils.customview
 
-class CustomButton {
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+
+class CustomButton @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = androidx.appcompat.R.attr.buttonStyle
+) : AppCompatButton(context, attrs, defStyleAttr) {
+
+    private var yOffset = 0f
+
+    init {
+        setOnTouchListener { _, event ->
+            when (event.action) {
+                android.view.MotionEvent.ACTION_DOWN -> {
+                    yOffset = 4 * resources.displayMetrics.density
+                    this.isPressed = true
+                    postInvalidate()
+                }
+                android.view.MotionEvent.ACTION_UP, android.view.MotionEvent.ACTION_CANCEL -> {
+                    yOffset = 0f
+                    this.isPressed = false
+                    postInvalidate()
+
+                    if (event.action == android.view.MotionEvent.ACTION_UP) {
+                        performClick()
+                    }
+                }
+            }
+            true // Return true to indicate that the listener has consumed the event.
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.save()
+        canvas.translate(0f, yOffset)
+        super.onDraw(canvas)
+        canvas.restore()
+    }
 }
--- app/src/main/java/com/cpp/unsmoke/utils/customview/EditText.kt
+++ app/src/main/java/com/cpp/unsmoke/utils/customview/EditText.kt
@@ -1,4 +1,13 @@
 package com.cpp.unsmoke.utils.customview
 
-class EditText {
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class EditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+): AppCompatEditText(context, attrs){
+    init {
+
+    }
 }
