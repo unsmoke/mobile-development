c233b5f96920a8ea607817621fb33316781750ec
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-05-21 19:16:36 +0700

    feat: add logic to the button

--- app/src/main/java/com/cpp/unsmoke/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/cpp/unsmoke/ui/auth/login/LoginActivity.kt
@@ -1,18 +1,57 @@
 package com.cpp.unsmoke.ui.auth.login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.cpp.unsmoke.R
+import com.cpp.unsmoke.databinding.ActivityLoginBinding
+import com.cpp.unsmoke.ui.auth.forgotpassword.ForgotPasswordActivity
+import com.cpp.unsmoke.ui.auth.register.RegisterActivity
+import com.cpp.unsmoke.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        binding.btnLogin.setOnClickListener {
+            toHome()
+        }
+
+        binding.btnSignup.setOnClickListener {
+            toSignUp()
+        }
+
+        binding.btnForgotPassword.setOnClickListener {
+            toForgotPassword()
+        }
+
+        binding.btnSigninWithGoogle.setOnClickListener {
+            toSignInWithGoogle()
+        }
+    }
+
+    private fun toSignInWithGoogle() {
+        TODO("Not yet implemented")
+    }
+
+    private fun toForgotPassword() {
+        val intent = Intent(this@LoginActivity, ForgotPasswordActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun toSignUp() {
+        val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun toHome() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        TODO("Add Finish Method In Future")
     }
 }
