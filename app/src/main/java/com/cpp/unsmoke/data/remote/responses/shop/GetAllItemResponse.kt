b9c1a2b73801faf89eb3283044b7a5b810f1acc9
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-06-12 23:29:02 +0700

    feat: init user repository and leaderboard repository

---
+++ app/src/main/java/com/cpp/unsmoke/data/remote/responses/shop/GetAllItemResponse.kt
@@ -0,0 +1,45 @@
+package com.cpp.unsmoke.data.remote.responses.shop
+
+import com.google.gson.annotations.SerializedName
+
+data class GetAllItemResponse(
+
+	@field:SerializedName("code")
+	val code: Int? = null,
+
+	@field:SerializedName("data")
+	val data: Data? = null,
+
+	@field:SerializedName("status")
+	val status: String? = null
+)
+
+data class Data(
+
+	@field:SerializedName("items")
+	val items: List<ItemsItem?>? = null
+)
+
+data class ItemsItem(
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String? = null,
+
+	@field:SerializedName("item_id")
+	val itemId: String? = null,
+
+	@field:SerializedName("img_url")
+	val imgUrl: String? = null,
+
+	@field:SerializedName("price")
+	val price: Int? = null,
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("description")
+	val description: String? = null,
+
+	@field:SerializedName("created_at")
+	val createdAt: String? = null
+)
--- app/src/main/java/com/cpp/unsmoke/repository/LeaderboardRepository.kt
+++ app/src/main/java/com/cpp/unsmoke/repository/LeaderboardRepository.kt
@@ -3,9 +3,8 @@ package com.cpp.unsmoke.repository
 import com.cpp.unsmoke.data.local.preferences.LoginPreferences
 import com.cpp.unsmoke.data.remote.retrofit.ApiService
 
-class ShopRepository(
+class LeaderboardRepository(
     private var apiService: ApiService,
     private val loginPreferences: LoginPreferences
 ) {
-
 }
